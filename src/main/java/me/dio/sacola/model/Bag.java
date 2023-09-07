a2291c6428a9d3d58bc3c7d68a29143761ec9d90
Diego <diego.rocha@edu.unipar.br>
2023-09-07 00:30:24 -0300

    feat: created domain classes

---
+++ src/main/java/me/dio/sacola/model/Bag.java
@@ -0,0 +1,33 @@
+package me.dio.sacola.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.dio.sacola.enumeration.FormPayment;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+public class Bag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JsonIgnore
+    private Client client;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Item> items;
+    private Double amount;
+    @Enumerated
+    private FormPayment formPayment;
+    private boolean closed;
+}
